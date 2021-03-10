882cb2bf529251b91cb235b1601ae7766e9a1cdf
Martin Thomas <marttho2@cisco.com>
2021-03-09 18:46:28 -0600

    Turn off lights after two mins if no change in distance

--- Display.h
+++ Display.h
@@ -3,13 +3,15 @@
 
 class Display{
   private:
+  
     bool alert_state = false;
-    size_t alert_counter = 0;
-    size_t alert_counter_max = 240;
+    size_t display_counter = 0;
+    size_t display_counter_max = 240;
     size_t alert_beats = 0; 
     const unsigned alert_beats_initial = 2;
     unsigned msd = 30;
-
+    size_t last_magnitude = 0;
+    
   public:
     Display(const unsigned minimum_safe_distance){
       msd = minimum_safe_distance;
@@ -41,36 +43,48 @@ class Display{
     }
     
     void displayGraph(unsigned magnitude, bool alerting){
+
+      bool display_on = true;
+      CircuitPlayground.strip.clear();      
+      uint32_t color = CircuitPlayground.strip.Color(255, 0, 0); //default is red
+      
       if (magnitude > 10){
         magnitude = 10;
       }
 
-      uint32_t color = CircuitPlayground.strip.Color(255, 0, 0); //default is red
-      CircuitPlayground.strip.clear();
-      
-      if (alerting){
-        alert_counter ++;
-        if (alert_counter > alert_counter_max){  // turn off alert light if on without interruption for 1 mins
+      if (last_magnitude != magnitude)
+      {
+        last_magnitude = magnitude;
+        display_counter = 0;
+      } else { // same distance, update counter and check limit
+        display_counter ++;
+        if (display_counter > display_counter_max){  // turn off light if on without interruption for 1 mins
           color = CircuitPlayground.strip.Color(0, 0, 0);
-        }
-        
-        if (alert_beats){ // turn on and off 
-          alert_beats--;
-        } else {
-          color = CircuitPlayground.strip.Color(0, 0, 0);
-          alert_beats = alert_beats_initial;
-        }
-      } else {  // no longer alerting, reset beats and counter
-        alert_counter = 0;
-        color = CircuitPlayground.strip.Color(0, 255, 0);
-        if (magnitude <5){
-         color = CircuitPlayground.strip.Color(0, 255, 255);
+          display_on = false;
         }
       }
-      for(size_t idx =0; idx <= magnitude; idx++){
-        CircuitPlayground.strip.setPixelColor(idx, color);
+
+      // pick color to display depending on if alerting or not
+      if (display_on){
+        if (alerting){
+          if(alert_beats){ // turn on and off 
+            alert_beats--;
+          } else {
+            color = CircuitPlayground.strip.Color(0, 0, 0);
+            alert_beats = alert_beats_initial;
+          }
+        } else {        
+          color = CircuitPlayground.strip.Color(0, 255, 0);
+          if (magnitude <5){
+           color = CircuitPlayground.strip.Color(0, 255, 255);
+          }
+        }
+
+        for(size_t idx =0; idx <= magnitude; idx++){
+          CircuitPlayground.strip.setPixelColor(idx, color);
+        }
+        CircuitPlayground.strip.show();      
       }
-      CircuitPlayground.strip.show();      
     }
 };
 #endif
